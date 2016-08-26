835838f3ad5ad1f55732e32910b315c64442c67a
Mike-Gough <Mrg14r@gmail.com>
2016-08-26 16:16:26 +1000

    Magic

--- src/main/scala/au.gov.csc.snippet/step.scala
+++ src/main/scala/au.gov.csc.snippet/step.scala
@@ -6,14 +6,16 @@ import scala.xml.NodeSeq
 import net.liftweb.util.Helpers._
 import net.liftweb.http.SHtml.ajaxSubmit
 import net.liftweb.http.js.JE.JsRaw
-import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.{JsCmd, JsCmds}
 import net.liftweb.http.js.JsCmds.SetHtml
+import xml.Text
 
 import scala.util.Random
 
 object step extends Loggable {
   var (step: Int, numberOfSteps: Int, numberOfQuestionsPerPage: Int) = (0, 4, 3)
   var serviceNumber: String = ""
+  var title : String = ""
 
   def render = {
 
@@ -21,11 +23,144 @@ object step extends Loggable {
       Thread.sleep(500 + Random.nextInt(3000))
       incrementStep
 
-      SetHtml("step-form", route)
+      SetHtml("step-form", route) &
+      JsCmds.Run("jQuery('#li-step-1').removeClass('disabled').removeClass('active').addClass('" + step1state + "')") &
+      JsCmds.Run("jQuery('#li-step-2').removeClass('disabled').removeClass('active').addClass('" + step2state + "')") &
+      JsCmds.Run("jQuery('#li-step-3').removeClass('disabled').removeClass('active').addClass('" + step3state + "')") &
+      SetHtml("heading-title", Text(heading)) &
+      SetHtml("verify-step-number", Text(verifyStepTitle))
     }
 
     "#step-form" #> route &
-        "type=submit" #> ajaxSubmit("Next", process)
+        "#li-step-1 [class]" #> step1state &
+          "#li-step-2 [class]" #> step2state &
+            "#li-step-3 [class]" #> step3state &
+              "type=submit" #> ajaxSubmit("Next", process)
+  }
+
+  def verifyStepTitle: String = {
+    if (step != 0 & step != numberOfSteps)
+      "Step " + verifyCurrentStepInProcess + " of " + verifyStepsInProcess
+    else
+      ""
+  }
+
+
+  def verifyCurrentStepInProcess: Int = {
+    numberOfSteps match {
+      case 3 => step
+      case 4 =>
+        if (ValidateServiceNumber.serviceNumber == "1")
+          1
+        else if (ValidateServiceNumber.serviceNumber == "0")
+          step
+        else
+          step
+      case 8 => step
+    }
+  }
+
+  def verifyStepsInProcess: Int = {
+    numberOfSteps match {
+      case 3 => 2
+      case 4 =>
+          if (ValidateServiceNumber.serviceNumber == "1")
+            1
+          else if (ValidateServiceNumber.serviceNumber == "0")
+            2
+          else
+            3
+      case 8 => 7
+    }
+  }
+
+  def heading: String = {
+
+    if (numberOfSteps == 3) {
+      if (step == 0) {
+        title = "Provide a membership number in addition to an email address or phone number to provision an account."
+      }
+      if (step == 1) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN."
+      }
+      if (step == 2) {
+        title = "To finish, enter the verification code sent to your nominated device. Contact the Customer Information Center (CIC) if no code is received within the next 5 minutes."
+      }
+      if (step == 3) {
+        title = "Your account has been successfully provisioned."
+      }
+    }
+
+    if (numberOfSteps == 4) {
+      if (step == 0) {
+        title = "Provide a membership number in addition to an email address or phone number to provision an account."
+      }
+      if (step == 1) {
+        title = "Some additional details are required to verify your identity. Please provide answers to the following set of questions about yourself. Set 1 of 2."
+      }
+      if (step == 2) {
+        title = "Some additional details are required to verify your identity. Please provide answers to the following set of questions about your last period of employment. Set 2 of 2."
+      }
+      if (step == 3) {
+        title = "To finish, enter the verification code sent to your nominated device. Contact the Customer Information Center (CIC) if no code is received within the next 5 minutes."
+      }
+      if (step == 4) {
+        title = "Your account has been successfully provisioned."
+      }
+    }
+
+    if (numberOfSteps == 8) {
+      if (step == 0) {
+        title = "Provide a membership number in addition to an email address or phone number to provision an account."
+      }
+      if (step == 1) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 1 of 6."
+      }
+      if (step == 2) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 2 of 6."
+      }
+      if (step == 3) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 3 of 6."
+      }
+      if (step == 4) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 4 of 6."
+      }
+      if (step == 5) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 5 of 6."
+      }
+      if (step == 6) {
+        title = "Some additional details are required to verify your identity. You must provide 5/6 correct answers in order to be issued a PIN. Click next to skip a question. Question 6 of 6."
+      }
+      if (step == 7) {
+        title = "To finish, enter the verification code sent to your nominated device. Contact the Customer Information Center (CIC) if no code is received within the next 5 minutes."
+      }
+      if (step == 8) {
+        title = "Your account has been successfully provisioned."
+      }
+    }
+
+    title
+  }
+
+  def step1state: String = {
+    if (step == 0)
+      "active"
+    else
+      "disabled"
+  }
+
+  def step2state: String = {
+    if (step != 0 & step != numberOfSteps)
+      "active"
+    else
+      "disabled"
+  }
+
+  def step3state: String = {
+    if (step == numberOfSteps)
+      "active"
+    else
+      "disabled"
   }
 
   // TODO - make this route to the page we want based on state
